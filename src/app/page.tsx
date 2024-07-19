5abf82462fd6e30e36c8b612bf2f30f00ea0ed8d
yeabalex <yeabalex18@gmail.com>
2024-07-19 03:43:07 -0700

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,92 +1,9 @@
 "use client";
 import React from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
+import { products } from "@/constants/products";
 
 
 export default function HeroParallaxDemo() {
   return <HeroParallax products={products} />;
 }
-export const products = [
-  {
-    title: "Moonbeam",
-    link: "/",
-    thumbnail: "/cvs/16.png",
-  },
-  {
-    title: "Cursor",
-    link: "/",
-    thumbnail: "/cvs/15.png",
-  },
-  {
-    title: "Rogue",
-    link: "https://userogue.com",
-    thumbnail: "/cvs/14.png",
-  },
-  {
-    title: "Editorially",
-    link: "https://editorially.org",
-    thumbnail: "/cvs/13.png",
-  },
-  {
-    title: "Editrix AI",
-    link: "https://editrix.ai",
-    thumbnail: "/cvs/12.png",
-  },
-  {
-    title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
-    thumbnail: "/cvs/11.png",
-  },
-  {
-    title: "Algochurn",
-    link: "https://algochurn.com",
-    thumbnail: "/cvs/10.png",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail: "/cvs/9.png",
-  },
-  {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
-    thumbnail: "/cvs/8.png",
-  },
-  {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
-    thumbnail: "/cvs/7.png",
-  },
-  {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
-    thumbnail: "/cvs/6.png",
-  },
-  {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
-    thumbnail: "/cvs/5.png",
-  },
-  {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
-    thumbnail: "/cvs/4.png",
-  },
-  {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
-    thumbnail: "/cvs/3.png",
-  },
-  {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
-    thumbnail: "/cvs/2.png",
-  },
-  {
-    title: "New Product",
-    link: "/",
-    thumbnail: "/cvs/1.png",
-  },
-];
-
-
