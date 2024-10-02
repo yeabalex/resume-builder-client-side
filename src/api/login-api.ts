2284383bda1a7104f6decb6019082dc06954e1c6
yeabalex <yeabalex18@gmail.com>
2024-10-02 10:55:52 -0700

    url2

--- src/api/login-api.ts
+++ src/api/login-api.ts
@@ -2,14 +2,7 @@ import apiUrl from "@/constants/api-url";
 import axios from "axios";
 
 export const loginUser = async (credentials: any) => {
-  let apiUrl;
 
-  const env = process.env.NODE_ENV;
-  if (env == "development") {
-    apiUrl = "http://localhost:3001";
-  } else if (env == "production") {
-    apiUrl = "https://kraftwerk.vercel.app";
-  }
   try {
     const loggedData = await axios.post(
       `${apiUrl}/api/auth/login`,
--- src/api/sign-up-api.ts
+++ src/api/sign-up-api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
-
+import apiUrl from '@/constants/api-url';
 export const sendData = async (data: any) => {
   try {
     console.log(data)
-    const sentData = await axios.post('http://localhost:3001/api/auth/signup', data);
+    const sentData = await axios.post(`${apiUrl}/api/auth/signup`, data);
     console.log(sentData.data); 
   } catch (err) {
     console.error(err);
