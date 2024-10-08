e023f579ffa8d93ac58b61ca0b22caecbf127b7d
Yeabsira A. <133959744+yeabalex@users.noreply.github.com>
2024-10-08 01:55:54 -0700

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy9TfnbvvU6OEuOFaLDy-OrUiESZfl0eM",
-  authDomain: "resume-builder-678ca.firebaseapp.com",
-  projectId: "resume-builder-678ca",
-  storageBucket: "resume-builder-678ca.appspot.com",
-  messagingSenderId: "266547010282",
-  appId: "1:266547010282:web:93fa8ffb5783d5ffaa5593"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
